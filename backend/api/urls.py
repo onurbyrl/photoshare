9a15b60110fb707728bb3dce68fd581cbb7e6485
Onur Bayirli <onurbayirli17@gmail.com>
2022-05-22 17:08:00 +0300

    files deleted

--- backend/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from api.views import PostList, PostCreate
-
-# app_name
-
-urlpatterns = [
-    path('', PostList.as_view(), name='postlist'),
-    path('create/', PostCreate.as_view(), name='postcreate'),
-]
